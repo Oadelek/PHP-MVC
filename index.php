16ac993845786a51fa9b197f725c34b4d8352b68
Oluwasesan Adeleke <oadeleke@algomau.ca>
2024-05-18 03:41:09 +0000

    Added redirection to log out

--- index.php
+++ index.php
@@ -1,11 +1,26 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] !== true) {
+    // User is not logged in, redirect to login page
+    header('Location: login.php');
+    exit();
+}
+
+// User is logged in
+$username = $_SESSION['username'];
+$currentDate = date('l, F jS, Y');
+
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>Welcome</title>
 </head>
 <body>
-    <h1>Assignment 1</h1>
-
-    <p><a href="/login.php">Click here to login</a></p>
+    <h1>Welcome, <?php echo $username; ?></h1>
+    <p>Today is: <?php echo $currentDate; ?></p>
+    <a href="logout.php">Logout</a>
 </body>
 </html>
--- validate.php
+++ validate.php
@@ -1,15 +1,28 @@
 <?php
 
+session_start();
+
 $valid_username = "mike";
 $valid_password = "password";
 
 $username = $_REQUEST['username'];
+$_SESSION['username'] = $username;
+
 $password = $_REQUEST['password'];
 
 if ($username == $valid_username && $password == $valid_password) {
-  echo "pass";
+  $_SESSION['authenticated'] = true;
+  $_SESSION['failed_attempts'] = 0; // Reset failed attempts
+    header('location: /');
 } else {
-  echo "fail";
+  if (!isset($_SESSION['failed_attempts'])) {
+      $_SESSION['failed_attempts'] = 1;
+  } else {
+      $_SESSION['failed_attempts']++;
+  }
+
+  echo "This is unsuccessful attempt number " . $_SESSION['failed_attempts'];
+  
 }
 
 
